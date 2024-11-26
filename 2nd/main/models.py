906721099fe740b9cd0a6339ae35f2e397a2a333
Xcient607 <madnanimran2@gmail.com>
2024-11-26 12:03:27 +0000

    Delete 2nd/main directory

--- 2nd/main/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-
-from typing import Optional
-from pydantic import BaseModel
-
-class ProjectData(BaseModel):
-    description: str
-    test_description: Optional[str] = None
-    # Add other fields if necessary
